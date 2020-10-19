c4a39537abf19fb406804111e71cd49504448a8e
aaron wesolek <maxwes13@gmail.com>
2020-10-18 22:05:06 -0500

    built up to needing use for reducer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
-
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp'
+import { Route, useLocation } from 'react-router-dom'
 
 
 function App() {
   return (
     <div className="App">
+      <Route path="/signin">
+        <SignIn />
+      </Route>
+      {/* <Route exact path='/'>
+        <Home />
+      </Route>
+      <Route path='/about'>
+        <About />
+      </Route> */}
+      <div className="App">
+        <Route exact path="/signup">
+          <SignUp />
+        </Route>
 
+        {/* <PrivateRoute path='/user' component={UserPage} /> */}
+      </div>
     </div>
   );
 }
---
+++ src/utilities/Styles.js
@@ -0,0 +1,94 @@
+const { makeStyles } = require("@material-ui/core");
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(0),
+    },
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: 200,
+    }
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    textAlign: 'center',
+  },
+  songTitle: {
+    fontSize: '1.5rem',
+    width: '400px',
+    overflow: 'hidden',
+    whiteSpace: 'nowrap'
+  },
+  songArtist: {
+    fontSize: '.75rem'
+  },
+  popover: {
+    pointerEvents: 'none',
+  },
+  gridList: {
+    width: '100%',
+    height: '80vh',
+    [theme.breakpoints.down('md')]: {
+      height: '100%'
+    }
+  },
+  playlist: {
+    [theme.breakpoints.down('md')]: {
+      display: 'none'
+    }
+  },
+  mobilePlaylist: {
+    [theme.breakpoints.up('lg')]: {
+      display: 'none'
+    }
+  },
+  modal: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+  songMedia: {
+    height: '100%',
+    paddingTop: '100%'
+  },
+  editText: {
+    width: '275px'
+  },
+  editTextFontSize: {
+    fontSize: '2.5rem',
+    textAlign: 'center'
+  },
+  playlistButtons: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    [theme.breakpoints.down('md')]: {
+      position: 'relative',
+      bottom: 0,
+      marginTop: '10px'
+    }
+  }
+}))
+
+export default useStyles
